f753654cb12ea2969654e16270f8fb0eb5d9dea5
grem62 <gremontmatheo22@gmail.com>
2023-02-17 02:27:54 -0800

    step65

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - fonction principal void = 0 argument
-*
-* Return: Always 0 (success)
-*/
+ *main - fonction principal void = zero argument
+ *sizeof - size of file in octet
+ *
+ *Return: always 0
+ */
+
 int main(void)
 {
-printf("Size of char:%zu byte(s)\n", sizeof(char));
-printf("Size of int:%zu byte(s)\n", sizeof(int));
-printf("Size of long int:%zu byte(s)\n", sizeof(long long int));
-printf("Size of long long int:%zu byte(s)\n", sizeof(long int));
-printf("Size of float:%zu byte(s)\n", sizeof(float));
-return (0);
+	printf("Size of a char:%2d byte(s)\n", sizeof(char));
+	printf("Size of an int:%2d byte(s)\n", sizeof(int));
+	printf("Size of a long int:%2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%2d byte(s)\n", sizeof(long long int));
+	printf("Size of a float:%2d byte(s)\n", sizeof(float));
+	return (0);
 }
