6f599f0871991bb153def9940ac06500d38af37f
grem62 <gremontmatheo22@gmail.com>
2023-02-17 06:30:00 -0800

    step101111

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return:1
- */
+* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+* followed by a new line, to the standard error
+*
+* Return: always 1
+*/
 int main(void)
 {
-	write(2, 58 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
