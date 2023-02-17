7aa65bf2d30a875be6959894b10bc8ed2918ba8b
grem62 <gremontmatheo22@gmail.com>
2023-02-17 01:14:09 -0800

    step41

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-main()
-
+/**
+* main - fonction principal void = 0 argument
+*
+* Return: Always 0 (success)
+*/
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n)
-	return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
