ccacb925c21cd18b115b2c1d568cd64e72c44010
grem62 <gremontmatheo22@gmail.com>
2023-02-17 01:57:16 -0800

    step6

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - fonction principal void = 0 argument
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("Size of char: %d\n", sizeof(char));
+printf("\nSize of int: %d\n", sizeof(int));
+printf("\nSize of float: %d\n", sizeof(float));
+printf("\nSize of long int: %d\n", sizeof(long int));
+printf("\nSize of double: %d\n", sizeof(double));
+return (0);
+}
