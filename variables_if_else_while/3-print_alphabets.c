055f82f92fee5dab0a419d4d0d372a3768b9d92a
grem62 <gremontmatheo22@gmail.com>
2023-02-20 13:44:29 -0800

    step3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+**/
+
+	int main(void)
+
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	a = 'A';
+
+	while (a <= 'Z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}	
