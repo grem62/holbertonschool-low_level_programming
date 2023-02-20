31a68b41255bfce4fcc21bae72544dadf212d81a
grem62 <gremontmatheo22@gmail.com>
2023-02-20 06:44:17 -0800

    step21

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+**/
+
+	int main(void)
+
+{
+	char ch = 'a';
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+
+		putchar(ch);
+
+	return (0);
+}
+
