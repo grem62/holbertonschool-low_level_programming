5c6da54fb6395c11b9b60f17c6c2dc2ba0073b61
grem62 <gremontmatheo22@gmail.com>
2023-02-22 14:34:02 -0800

    step10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - ajouter deux character
+ *@a: premier char
+ *@b: dexieme char
+ * Return: somme des char
+**/
+
+	int add(int a, int b)
+{
+
+		return (a + b);
+}
