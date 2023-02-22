5821198e2f78d7597ea1d494c018f4c73d511530
grem62 <gremontmatheo22@gmail.com>
2023-02-22 04:19:37 -0800

    step6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_abs - Write a function that computes the absolute value of an integer
+*@n: character to be verified
+*Return: return always 0
+*/
+
+	int _abs(int n)
+{
+	if (n > 0)
+	return (n);
+
+	else
+
+	return (-n);
+}
