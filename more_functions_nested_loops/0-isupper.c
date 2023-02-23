00b196c81333d5539f2f00779831f907e820d53b
grem62 <gremontmatheo22@gmail.com>
2023-02-23 06:35:34 -0800

    step0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_isupper - veriffier si un caractere est une lettre majuscule
+*@c: charachter
+*Return: 1 or 0
+**/
+	int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}	
