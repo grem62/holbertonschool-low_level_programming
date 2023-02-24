6ae7ce5d5961fad7ef46d467bc284e654e442056
grem62 <gremontmatheo22@gmail.com>
2023-02-24 06:03:40 -0800

    step8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints a square
+ *@size: character
+**/
+void print_square(int size)
+
+{
+		int c;
+		int l;
+	if (size > 0)
+	{
+		for (c = 0; c <= size; c++)
+			{
+			for (l = 0; l <= size; l++)
+				{
+				_putchar('#');
+				}
+				_putchar('\n');
+			}
+	}
+		else
+		{
+
+			_putchar('\n');
+		}
+}
