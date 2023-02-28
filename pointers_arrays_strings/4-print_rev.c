8aa6d7569249612df48d330940b96e38338cc041
grem62 <gremontmatheo22@gmail.com>
2023-02-28 05:01:49 -0800

    step4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - function that prints a string, followed by a new line
+ * @s: character
+ */
+
+	void print_rev(char *s)
+
+{
+	char *p = s;
+
+		while (*p != '\0')
+		{
+			p++;
+		}
+		while (p >= s)
+			{
+			putchar(*p--);
+
+
+			}
+		putchar('\n');
+}
