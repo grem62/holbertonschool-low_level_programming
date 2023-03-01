f5ca8e5c08acdbdc35e7308ea0b22a8c7d12507a
grem62 <gremontmatheo22@gmail.com>
2023-03-01 05:19:41 -0800

    step8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - Write a function that prints n elements of an array
+ *@a: character
+ *@n: character 2
+**/
+
+	void print_array(int *a, int n)
+{
+		int index;
+
+		for (index = 0; index < n ; index++)
+		{
+			printf("%d", a[index]);
+
+				printf(",");
+
+				printf(" ");
+		}
+			printf("\n");
+
+}
