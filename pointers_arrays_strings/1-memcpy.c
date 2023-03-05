a75278ccb0a4a5ff782e5f7413f42943f611295e
grem62 <gremontmatheo22@gmail.com>
2023-03-05 09:51:19 -0800

    step1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies byte zone
+ * @dest: character
+ * @src: character1
+ * @n: character 2
+ *
+ * Return: return value
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+
+	return (dest);
+}
