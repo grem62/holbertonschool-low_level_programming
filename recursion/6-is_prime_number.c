3efa3df0afdd36e2b35b3fb007ed43d64a5db77f
grem62 <gremontmatheo22@gmail.com>
2023-03-07 02:02:43 -0800

    step6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *is_find_number -fonction
+ *@n: character
+ *Return: 0 or 1
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+	return (0);
+
+		return (1);
+	
+}
