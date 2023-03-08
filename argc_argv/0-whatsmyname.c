b96ac0159a5c50f8f756fbbba91a7bc3896188a4
grem62 <gremontmatheo22@gmail.com>
2023-03-08 04:07:14 -0800

    step0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(int argc, char *argv[]) 
+/**
+ *main - fonction
+ *@argc: character
+ *@argv: character2
+ *Return: 0
+*/
+
+
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-    printf("%s\n", argv[0]);
-    return 0;
+		printf("%s\n", argv[0]);
+	return (0);
 }
