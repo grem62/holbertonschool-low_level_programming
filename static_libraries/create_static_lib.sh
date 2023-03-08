ac55a5b982ee88c909d16e0c7eae466cfa315603
grem62 <gremontmatheo22@gmail.com>
2023-03-08 01:31:12 -0800

    step1

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# Compile all the .c files in the current directory into object files
 gcc -c *.c
-
-# Create the static library from the object files
-ar rcs liball.a *.o
+ar rc liball.a *.o
+ranlib liball.a
