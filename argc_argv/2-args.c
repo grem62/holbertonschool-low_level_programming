cb1aead19649426ee7f440cf4bb15b3efe4d1903
grem62 <gremontmatheo22@gmail.com>
2023-03-08 04:39:06 -0800

    step2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+	int main(int argc, char* argv [])
+{
+	int i = 1;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
