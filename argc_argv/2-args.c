c2809c0cc1a6bde45ab4e74f406256745595da08
grem62 <gremontmatheo22@gmail.com>
2023-03-08 04:43:05 -0800

    step2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,16 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ *main - fonction
+ *@argc: character 1
+ *@argv: character 2
+ *Return: 0
  */
 
-	int main(int argc, char* argv [])
+	int main(int argc, char *argv[])
 {
 	int i = 1;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
