f610585e6f474583bd0a955255e1a947c5beaf65
grem62 <gremontmatheo22@gmail.com>
2023-03-14 03:22:10 -0700

    step1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,25 +9,28 @@
  *Return: NULL
  */
 
-
-	char *_strdup(char *str)
+char *_strdup(char *str)
 {
 	char *copy;
-	int len = 0
 
-	;if (str == NULL)
-	{
+	int index;
+	int len = 0;
+
+	if (str == NULL)
 		return (NULL);
-	}
+
+	for (index = 0; str[index]; index++)
 		len++;
 
-	;copy = malloc(sizeof(char)*(len + 1));
+	copy = malloc(sizeof(char) * (len + 1));
 
 	if (copy == NULL)
 		return (NULL);
 
-	strcpy(copy, str);
+	for (index = 0; str[index]; index++)
+		copy[index] = str[index];
+
+	copy[len] = '\0';
 
 	return (copy);
-	free(copy);
 }
