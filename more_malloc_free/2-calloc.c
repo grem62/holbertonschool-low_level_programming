bea183a0ed4547fdf373ef3878bfa949cb4800b2
grem62 <gremontmatheo22@gmail.com>
2023-03-16 05:46:19 -0700

    step2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - function 
+ * @nmemb: character
+ * @size: character 2
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int count;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	count = 0;
+	while (count < nmemb * size)
+	{
+		ptr[count] = 0;
+		count++;
+	}
+	return (ptr);
+}
