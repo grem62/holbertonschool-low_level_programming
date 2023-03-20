fd0405f6025232661a6c8d212c7d4b6cb8f6aee2
grem62 <gremontmatheo22@gmail.com>
2023-03-20 02:23:56 -0700

    step0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *struct dog - description dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
