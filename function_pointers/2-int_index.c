92e77b6a9fd7af1ce70bb56b2c34407a05da0a41
grem62 <gremontmatheo22@gmail.com>
2023-03-22 03:28:23 -0700

    step2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/*
+ *
+ *
+ *
+ */
+
+	int int_index(int *array, int size, int (*cmp)(int))
+{	
+	int index = 0;
+
+	if (size <= 0)
+	return (-1);
+
+	if (array == NULL || cmp == NULL)
+	return (-1);
+
+	for (; index < size; index++)
+
+	if (cmp(array[index]) != 0)
+	return (index);
+
+	return (-1);
+
+}
