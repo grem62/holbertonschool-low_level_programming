c5b9a2fabd7dfd7cbb2ccc31d038f64488a9a008
grem62 <gremontmatheo22@gmail.com>
2023-03-23 14:11:57 +0100

    [ADD] File 1-print_numbers and header file variadic_function for task 1 of project 3, variadic function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,9 @@
-#ifndef VARIA_FUN
-#define VARIA_FUN
+#ifndef VARIA_FUN_H_
+#define VARIA_FUN_H_
+#include <stdarg.h>
 
 void _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
