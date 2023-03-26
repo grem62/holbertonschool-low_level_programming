563056821f17ac01603dc9352010373ba599af5e
grem62 <gremontmatheo22@gmail.com>
2023-03-26 21:45:51 +0200

    step2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,35 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
- *print_strings - fonction print string
- *@separator: separateur
- *@n: constante n
- **/
+ * print_strings - main function that print a string
+ * @separator: separation beetween two string
+ * @n: contain the two string
+*/
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list pn;
-	unsigned int index = 0;
-	char *str;
+	va_list strg;
 
-	va_start(pn, n);
+	char *str;
+	unsigned int index = 0;
+
+	va_start(strg, n);
+
 	for (; index < n; index++)
 	{
-		str = va_arg(pn, char *);
-		if (str == NULL)
-		printf("(nil)");
-		else
-		printf("%s", str);
-
-		if (index < n - 1 && separator != NULL)
-		printf("%s", separator);
+		str = va_arg(strg, char *);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+			if (index < n - 1 && separator != NULL)
+				printf("%s", separator);
 	}
-
-	va_end(pn);
-
-	putchar('\n');
-
+	putchar ('\n');
 }
