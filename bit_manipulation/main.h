5316de4627907d6ee71868214a7eafa0bc2753a2
grem62 <gremontmatheo22@gmail.com>
2023-04-07 22:26:48 +0200

    print binary number

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,8 +1,12 @@
 #ifndef __MAIN_H_
 #define __MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
-int _putchar(char c);
+int  _putchar(char c);
+void print_binary(unsigned long int n);
 
 #endif
 
