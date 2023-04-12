a157bc386d935aab5c9fe8522c1eb0fa4fcbaece
grem62 <gremontmatheo22@gmail.com>
2023-04-12 11:05:34 +0200

    uptdate

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,30 +9,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w;
-	int len;
+	int openfile, writefile;
 
-	if (!filename)
-	{
+	if (filename == NULL)
+		return (0);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	openfile = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+
+	writefile = write(openfile, text_content, strlen(text_content));
+
+	if (writefile == -1 || openfile == -1)
 		return (-1);
-	}
 
-	if (!text_content)
-	{
-		text_content = " ";
-	}
-
-	len = strlen(text_content);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (fd == -1)
-	{
-		return (-1);
-	}
-	w = write(fd, text_content, len);
-	if (w == -1)
-	{
-		return (-1);
-	}
 	return (1);
 }
