d688e2567b9c5359d866056409fbce970a4573b9
grem62 <gremontmatheo22@gmail.com>
2023-05-24 10:25:19 +0200

    add the task 0 and the header file

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ *linear_search - function search the value in linear table
+ *@array: table
+ *@value: value in a table
+ *Return: the value or void
+ *@size: size of array
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index;
+
+	if (array == NULL)
+	{
+	return (-1);
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		printf("Value checked array[%lu] = %d\n", index, array[index]);
+
+		if (array[index] == value)
+		{
+		return (index);
+		}
+	}
+	return (-1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef __ALGO__
+#define __ALGO__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
